56d7f4c85735dfdf1f097b3995ad9b9f61a0b79c
brkdli <brkdeli@outlook.com.tr>
2026-03-14 22:58:00 +0300

    Fix CSS styling bug on mobile devices

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-14T19:57:49.684Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-03-14T19:58:00.299Z
+Commit: Fix CSS styling bug on mobile devices
