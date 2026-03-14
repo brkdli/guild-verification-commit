21dc843868bcfd4d9241afcd47b3c0eb1193dc58
brkdli <brkdeli@outlook.com.tr>
2026-03-14 23:25:32 +0300

    Fix race condition in async operations

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-14T20:25:26.427Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-14T20:25:32.172Z
+Commit: Fix race condition in async operations
