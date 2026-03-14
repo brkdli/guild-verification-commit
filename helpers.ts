4d07db7002d55ffea2a2e0301dffa83c13cb83d8
brkdli <brkdeli@outlook.com.tr>
2026-03-14 23:25:52 +0300

    Patch API endpoint error handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-14T20:25:37.547Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-03-14T20:25:51.901Z
+Commit: Patch API endpoint error handling
