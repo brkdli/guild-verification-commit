50b3f488a05adf2005f5bdc314ba368e1e161d9f
brkdli <brkdeli@outlook.com.tr>
2026-03-14 23:26:11 +0300

    Fix memory leak in event handlers

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-14T20:25:38.839Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-14T20:26:11.503Z
+Commit: Fix memory leak in event handlers
