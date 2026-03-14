1338a8a6b97c0ec82ccf1945fcd52f5d40bc14d8
brkdli <brkdeli@outlook.com.tr>
2026-03-14 23:26:41 +0300

    Resolve timezone issues in date handling

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-14T20:26:15.071Z
-Commit: Add multi-language support
+Timestamp: 2026-03-14T20:26:41.303Z
+Commit: Resolve timezone issues in date handling
