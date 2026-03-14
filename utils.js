115c29cea0595978ce8823787d5a21cced9a4992
brkdli <brkdeli@outlook.com.tr>
2026-03-14 23:25:41 +0300

    Resolve null pointer exception in user service

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-14T19:57:21.794Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-14T20:25:41.252Z
+Commit: Resolve null pointer exception in user service
