aa72b96f90c7a6c79cfcd3f1d729c3c01ae40aac
brkdli <brkdeli@outlook.com.tr>
2026-03-14 23:25:59 +0300

    Resolve timezone issues in date handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-14T20:25:55.166Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2026-03-14T20:25:58.926Z
+Commit: Resolve timezone issues in date handling
